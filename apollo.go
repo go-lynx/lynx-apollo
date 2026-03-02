364b5b01133d13ba7639eabc79c0980858e1105a
tan-zhuo <tanzhuo@aliyun.com>
2026-03-02 21:51:56 +0900

    chore: bump pluginVersion to v1.5.4

--- apollo.go
+++ apollo.go
@@ -20,7 +20,7 @@ const (
 	pluginName = "apollo.config.center"
 
 	// pluginVersion represents the current version of the Apollo configuration center plugin.
-	pluginVersion = "v1.0.0"
+	pluginVersion = "v1.5.4"
 
 	// pluginDescription briefly describes the functionality of the Apollo configuration center plugin.
 	pluginDescription = "apollo configuration center plugin for lynx framework"
